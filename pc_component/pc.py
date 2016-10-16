c4b3f364b6a0e0120da119b6ce61bf79fdef37c7
Chen Zhe <chenzhesg@gmail.com>
2016-10-17 00:37:28 +0800

    preliminary PC CLI interface

--- pc_component/pc.py
+++ pc_component/pc.py
@@ -1,35 +1,56 @@
 import socket
 import datetime
 import wave
+import threading
+import sys
 
-#record a single audio file of 8 channels
-seconds_to_record = 10
+class RecordStream(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.seconds_recorded = 0
+        self.continue_recording = False
 
-out_sound = wave.open('test5.wav', 'wb')
-# (num of channels, sampling width in bytes, sampling rate, num of frames, compression type, compression name)
-out_sound.setparams((8, 2, 16000, 0, 'NONE', 'not compressed'))
+    def run(self):
+        out_sound = wave.open('test.wav', 'wb')
+        # (num of channels, sampling width in bytes, sampling rate, num of frames, compression type, compression name)
+        out_sound.setparams((8, 2, 16000, 0, 'NONE', 'not compressed'))
 
-connection = socket.socket()
-connection.connect(("192.168.1.14", 8000))
+        connection = socket.socket()
+        connection.connect(("192.168.1.14", 8000))
 
-print "connection established, recording..."
+        print "connection established, recording..."
 
-expected_bytes = 16000*2*8 * seconds_to_record
-bytes_received = 0
+        expected_bytes = 16000 * 2 * 8 # 1 second
+        bytes_received = 0
 
-while bytes_received<expected_bytes:
-    file_buffer = connection.recv(4096) # it will not always receive 4096 bytes
+        while bytes_received<expected_bytes:
+            file_buffer = connection.recv(4096) # it will not always receive 4096 bytes
 
-    file_buffer_length = len(file_buffer)
-    bytes_received += file_buffer_length
+            file_buffer_length = len(file_buffer)
+            bytes_received += file_buffer_length
 
-    if bytes_received>=expected_bytes:
-        file_buffer = file_buffer[0:file_buffer_length - (bytes_received-expected_bytes)]
+            if bytes_received>=expected_bytes:
+                self.seconds_recorded +=1
+                sys.stdout.write(str(self.seconds_recorded) + " second recorded")
+                sys.stdout.write("\r")
+                if self.continue_recording:
+                    bytes_received = bytes_received-expected_bytes                    
+                else:
+                    file_buffer = file_buffer[0:file_buffer_length - (bytes_received-expected_bytes)]
     
-    out_sound.writeframes(file_buffer)
+            out_sound.writeframes(file_buffer)
 
-connection.close
-out_sound.close()
+        connection.close
+        out_sound.close()
 
-print str(seconds_to_record) + " second audio file received"
+        print str(self.seconds_recorded) + " second audio file saved"
 
+
+raw_input("Press Enter to start recording...")
+
+thread = RecordStream()
+thread.continue_recording = True
+thread.start()
+raw_input("Press Enter again to stop recording...\n")
+thread.continue_recording = False
+thread.join()

16c1946369b9700cebe24661ea8eabea155f2cdd
Chen Zhe <chenzhesg@gmail.com>
2017-02-20 21:26:42 +0800

    store final version of transcript and automatically open it after completion

--- pc_component/pc-transcript.py
+++ pc_component/pc-transcript.py
@@ -2,6 +2,7 @@ import socket
 import datetime
 import threading
 from ntpserver import ntpServer
+import os
 
 class DisplayTranscript(threading.Thread):
     def __init__(self):
@@ -11,7 +12,7 @@ class DisplayTranscript(threading.Thread):
 
     def run(self):
         utterance = 1
-        
+        text_file = open("transcript.txt", "w")
         self.connection.connect(("192.168.0.101", 8000))
 
         print "connection established, printing transcript\n"
@@ -30,11 +31,13 @@ class DisplayTranscript(threading.Thread):
 
                 if res[0] == '1':                
                     print "\r", str(utterance)+".", res[1:], "\n"
+                    text_file.write(str(utterance)+". "+res[1:]+"\n\n")
                     utterance+=1
                 else:
                     print "\r", res[1:],
         
         self.connection.close()
+        text_file.close()
 
 ntp = ntpServer()
 
@@ -48,3 +51,4 @@ thread.connection.send("S")
 thread.keep_alive = False
 ntp.stop()
 thread.join()
+os.system("transcript.txt")
